ff1f10d73c7713ba4f29ae96caa0b4084dbc4d0c
venkatesheee26@gmail.com <venkatesheee26@gmail.com>
2023-07-13 14:18:23 +0530

    ps4

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine("The number is positive.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("The number is negative.");
+        }
+        else
+        {
+            Console.WriteLine("The number is zero.");
+        }
+
+        string evenOdd = (number % 2 == 0) ? "even" : "odd";
+        Console.WriteLine("The number is " + evenOdd + ".");
+    }
+}
